cc1b15d4f57e7c53fb67db9766d8d7a47b88f077
sulistianengsih5 <sulistianengsih5@gmail.com>
2023-12-06 16:09:51 +0700

    initial commit

---
+++ app/src/main/java/com/example/stylesync/ui/login/LoginViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.stylesync.ui.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import android.util.Patterns
+import com.example.stylesync.ui.data.LoginRepository
+import com.example.stylesync.ui.data.Result
+
+class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel() {
+
+}
---
+++ app/src/main/java/com/example/stylesync/ui/onboarding/LogoOnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.stylesync.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.stylesync.R
+
+class LogoOnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logo_onboarding)
+    }
+}
